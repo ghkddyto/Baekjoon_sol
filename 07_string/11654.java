c932904f0b50886127c3426a9e9a921d56e9231f
ghkddyto <dyto153@naver.com>
2021-04-16 16:34:21 +0900

    백준 11654번 문제
    https://www.acmicpc.net/submit/11654

---
+++ 07_string/11654.java
@@ -0,0 +1,17 @@
+//알파벳 소문자, 대문자, 숫자 0-9중 하나가 주어졌을 때, 주어진 글자의 아스키 코드값을 출력하는 프로그램을 작성하시오.
+
+import java.io.*;
+
+public class Main{
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String aski = br.readLine();
+
+        bw.write((int)aski.toCharArray()[0] + "");
+        bw.flush();
+        bw.close();
+
+    }
+}
