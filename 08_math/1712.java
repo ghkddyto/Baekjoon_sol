a35fc338934e9e7014a6ef85b31a50a774750cc2
ghkddyto <dyto153@naver.com>
2021-04-27 16:11:40 +0900

    백준 1712번 문제
    https://www.acmicpc.net/problem/1712

---
+++ 08_math/1712.java
@@ -0,0 +1,31 @@
+//노트북 판매 대수에 상관없이 매년 임대료, 재산세, 보험료, 급여 등 A만원의 고정 비용이 들며,
+//한 대의 노트북을 생산하는 데에는 재료비와 인건비 등 총 B만원의 가변 비용이 든다고 한다.
+//노트북 가격이 C만원으로 책정되었다고 한다.
+//일반적으로 생산 대수를 늘려 가다 보면 어느 순간 총 수입(판매비용)이 총 비용(=고정비용+가변비용)보다 많아지게 된다.
+//최초로 총 수입이 총 비용보다 많아져 이익이 발생하는 지점을 손익분기점(BREAK-EVEN POINT)이라고 한다.
+//A, B, C가 주어졌을 때, 손익분기점을 구하는 프로그램을 작성하시오.
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        int C = Integer.parseInt(st.nextToken());
+
+
+        if(B >= C) bw.write("-1");
+
+        else bw.write(A/(C-B) + 1 + "");
+
+        bw.flush();
+        bw.close();
+
+    }
+}
