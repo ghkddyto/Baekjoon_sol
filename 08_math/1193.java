8157f4b6da6858c8cc7a3afc17e644c2b1f1c2e5
황요새 <62563127+ghkddyto@users.noreply.github.com>
2022-02-10 16:51:41 +0900

    s

--- 08_math/1193.java
+++ 08_math/1193.java
@@ -6,23 +6,21 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int X = in.nextInt();
-        int cross_count = 1, prev_count_sum = 0;
-        while (true) {
-            if (X <= prev_count_sum + cross_count) {
-                if (cross_count % 2 == 1) {
-                    System.out.print((cross_count - (X - prev_count_sum - 1)) + "/" + (X - prev_count_sum));
-                    break;
-                }
-                else {
-                    System.out.print((X - prev_count_sum) + "/" + (cross_count - (X - prev_count_sum - 1)));
-                    break;
-                }
-            } else {
-                prev_count_sum += cross_count;
-                cross_count++;
-            }
+        Scanner sc = new Scanner(System.in);
+
+        int X = sc.nextInt();
+
+        int n = 1;
+        while((n*n+n)/2 < X){
+            n++;
         }
+        int tmp = (n*n+n)/2;
+
+        int mo = 1 + tmp-X ;
+        int ja = 1+ n - mo;
+
+        if(n%2==1) System.out.println(mo+"/"+ja);
+        else System.out.println(ja+"/"+mo);
+    }
     }
 }
